326e4755356a8b1ac68294cd6ce5a35578a3992d
luoyafei <luoyafei.newinstance@foxmail.com>
2016-11-03 09:56:39 +0800

    add Order.java and ShopCar beans

---
+++ src/com/smacker/bean/Order.java
@@ -0,0 +1,102 @@
+package com.smacker.bean;
+
+import java.sql.Timestamp;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name="t_commodityOrder")
+public class Order {
+	
+	private String orderId = null;
+	private Commodity commodityId = null;	//商品
+	private int unitPrice = 0;				//单价
+	private int commodityCount = 0;			//个数
+	private User userId = null;				//买方
+	private User sellerId = null;			//卖方
+	private String addr = null;				//地址
+	private String status = "0";			//状态(0:订单未创建；1:订单成功；2:订单作废)
+	private Timestamp orderDate = null;
+	
+	@Id
+	@GenericGenerator(name="uuid", strategy="uuid")
+	@GeneratedValue(generator="uuid")
+	public String getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+	
+	/**
+	 * 一对一，获取商品
+	 * @return
+	 */
+	@OneToOne(cascade={CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH},fetch=FetchType.LAZY)
+	@JoinColumn(name="commodityId")
+	public Commodity getCommodityId() {
+		return commodityId;
+	}
+	public void setCommodityId(Commodity commodityId) {
+		this.commodityId = commodityId;
+	}
+	public int getUnitPrice() {
+		return unitPrice;
+	}
+	public void setUnitPrice(int unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+	public int getCommodityCount() {
+		return commodityCount;
+	}
+	public void setCommodityCount(int commodityCount) {
+		this.commodityCount = commodityCount;
+	}
+	
+	@ManyToOne(cascade={CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH},fetch=FetchType.EAGER)
+	@JoinColumn(name="userId")
+	public User getUserId() {
+		return userId;
+	}
+	public void setUserId(User userId) {
+		this.userId = userId;
+	}
+	
+	@ManyToOne(cascade={CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH},fetch=FetchType.EAGER)
+	@JoinColumn(name="sellerId")
+	public User getSellerId() {
+		return sellerId;
+	}
+	public void setSellerId(User sellerId) {
+		this.sellerId = sellerId;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public Timestamp getOrderDate() {
+		return orderDate;
+	}
+	public void setOrderDate(Timestamp orderDate) {
+		this.orderDate = orderDate;
+	}
+	
+}
---
+++ src/com/smacker/bean/ShopCar.java
@@ -0,0 +1,58 @@
+package com.smacker.bean;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name="t_shopCar")
+public class ShopCar {
+	
+	private String shopCarId = null;
+	private User userId = null;
+	private Commodity commodityId = null;
+	
+	
+	@Id
+	@GenericGenerator(name="uuid", strategy="uuid")
+	@GeneratedValue(generator="uuid")
+	public String getShopCarId() {
+		return shopCarId;
+	}
+	public void setShopCarId(String shopCarId) {
+		this.shopCarId = shopCarId;
+	}
+
+	/**
+	 * 获取购物车所属用户，将获取设置为懒加载
+	 * @return
+	 */
+	@OneToOne(cascade={CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH},fetch=FetchType.LAZY)
+	@JoinColumn(name="userId")
+	public User getUserId() {
+		return userId;
+	}
+	public void setUserId(User userId) {
+		this.userId = userId;
+	}
+	
+	/**
+	 * 获取购物车内的商品，将获取设置为懒加载
+	 * @return
+	 */
+	@OneToOne(cascade={CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH},fetch=FetchType.LAZY)
+	@JoinColumn(name="commodityId")
+	public Commodity getCommodityId() {
+		return commodityId;
+	}
+	public void setCommodityId(Commodity commodityId) {
+		this.commodityId = commodityId;
+	}
+}
