e2099b93a518954b8bc7c44ac900e07da136deb3
luoyafei <luoyafei.newinstance@foxmail.com>
2017-03-27 22:16:48 +0800

    test

--- src/com/smacker/action/CompleteUserInfo.java
+++ src/com/smacker/action/CompleteUserInfo.java
@@ -1,8 +1,14 @@
 package com.smacker.action;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
 import com.opensymphony.xwork2.ActionSupport;
 
 @SuppressWarnings("serial")
+@Component("completeUserInfo")
+@Scope("prototype")
 public class CompleteUserInfo extends ActionSupport {
 
+	
 }
